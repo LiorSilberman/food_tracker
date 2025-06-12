8b73c798f2da54351ec66ead06abe931121ef82f
LiorSilberman <103421210+LiorSilberman@users.noreply.github.com>
2025-06-12 18:18:46 +0300

    Use expo-config API_URL

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 import Constants from 'expo-constants';
 
+export const API_URL =
+  Constants?.expoConfig?.extra?.API_URL ?? 'http://192.168.1.102:5000';
 
-// export const API_URL = Constants?.expoConfig?.extra?.API_URL ?? 'http://192.168.1.102:5000';
-export const API_URL = 'https://b44c-87-71-186-184.ngrok-free.app';
